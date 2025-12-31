cfaee706f0fd615c8372021025f97dbe16b25106
christofuyy <christo1321youn@gmail.com>
2025-12-30 21:09:42 -0800

    Add ts_ls to lspconfig servers

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "gdscript" }
+local servers = { "html", "cssls", "gdscript", "ts_ls" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
